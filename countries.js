095136ad39f64a963f6d54be511a09bf6c9ed411
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2023-07-19 10:49:04 +0530

    Initial Commit

--- countries.js
+++
@@ -1,9 +0,0 @@
-const countries = {
-    "id-ID": "Indonesian",
-    "ar-SA": "Arabic",
-    "de-DE": "Greman",
-    "it-IT": "Italian",
-    "es-ES": "Spanish",
-    "ja-JP": "Japanese",
-    "en-GB": "English"
-};
